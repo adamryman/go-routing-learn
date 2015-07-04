0ca03ce0f9b751e22e4a3aa2bc5b5174b1610cbc
Adam Ryman <adamryman@gmail.com>
2015-07-04 01:25:09 -0700

    Added simple custom handler

--- main.go
+++ main.go
@@ -3,13 +3,29 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 )
+
+//custom type
+type timeHandler struct {
+	format string
+}
+
+// ServeHTTP for custom time which makes it a handler
+func (th *timeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	tm := time.Now().Format(th.format)
+	w.Write([]byte("The time is: " + tm))
+}
 
 func main() {
 	mux := http.NewServeMux()
 
 	rh := http.RedirectHandler("http://example.org", 307)
 	mux.Handle("/foo", rh)
+
+	//time
+	th := &timeHandler{format: time.RFC1123}
+	mux.Handle("/time", th)
 
 	log.Println("Listening...")
 	http.ListenAndServe(":3000", mux)
